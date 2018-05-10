221f1713140d59864c40618b76fb6274f28501d3
Jose M. Fernandez-Alba <josemaria@starzplayarabia.com>
2018-05-10 13:18:17 +0200

    Use correct url for healthcheck

--- health-check.sh
+++ health-check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CODE=$(curl -s -o /dev/null -w %{http_code} http://${MESOS_SLAVE_PID#*@}/)
+CODE=$(curl -s -o /dev/null -w %{http_code} http://${MESOS_AGENT_ENDPOINT%:*}:$PORT0/)
 
 [ $CODE -ge 200 -a $CODE -lt 300 -o $CODE -eq 503 ]
 
