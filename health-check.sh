0b72c1a0067281dabaf41b95dbe8893b2fae393f
Jose M. Fernandez-Alba <josemaria@starzplayarabia.com>
2018-05-10 15:39:09 +0200

    Debug code.

--- health-check.sh
+++ health-check.sh
@@ -1,7 +1,14 @@
 
 set -x
 
-CODE=$(curl -s -o /dev/null -w %{http_code} "http://${MESOS_AGENT_ENDPOINT%:*}:$PORT0/")
+echo $MESOS_AGENT_ENDPOINT
+echo ${MESOS_AGENT_ENDPOINT%:*}
+
+URL=${MESOS_AGENT_ENDPOINT%:*}
+
+echo $URL
+
+CODE=$(curl -s -o /dev/null -w %{http_code} "http://$URL:$PORT0/")
 
 [ $CODE -ge 200 -a $CODE -lt 300 -o $CODE -eq 503 ]
 
