815f93140abd32e4c48676f8d94ffd6a7a980e37
Jose M. Fernandez-Alba <josemaria@starzplayarabia.com>
2018-05-10 15:36:32 +0200

    Include double quotes in url

--- health-check.sh
+++ health-check.sh
@@ -1,7 +1,7 @@
 
 set -x
 
-CODE=$(curl -s -o /dev/null -w %{http_code} http://${MESOS_AGENT_ENDPOINT%:*}:$PORT0/)
+CODE=$(curl -s -o /dev/null -w %{http_code} "http://${MESOS_AGENT_ENDPOINT%:*}:$PORT0/")
 
 [ $CODE -ge 200 -a $CODE -lt 300 -o $CODE -eq 503 ]
 
