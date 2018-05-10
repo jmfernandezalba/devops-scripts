16207276ce24f448b1b3324bb91e79b450f5dfc5
Jose M. Fernandez-Alba <josemaria@starzplayarabia.com>
2018-05-10 15:55:41 +0200

    Set correct script

--- health-check.sh
+++ health-check.sh
@@ -1,16 +1,7 @@
 
 set -x
 
-echo $MESOS_AGENT_ENDPOINT
-echo ${MESOS_AGENT_ENDPOINT%:*}
-
-URL=${MESOS_AGENT_ENDPOINT%:*}
-
-echo $URL
-
-env
-
-CODE=$(curl -s -o /dev/null -w %{http_code} "http://$URL:$PORT0/")
+CODE=$(curl -s -o /dev/null -w %{http_code} "http://${MESOS_AGENT_ENDPOINT%:*}:$PORT0/")
 
 [ $CODE -ge 200 -a $CODE -lt 300 -o $CODE -eq 503 ]
 
