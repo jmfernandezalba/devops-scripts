f93ba5cfb77721b3c6e2b1a9da9261b23a1bf859
Jose M. Fernandez-Alba <josemaria@starzplayarabia.com>
2018-05-22 13:01:36 +0200

    Update script to be "bi-stable"

--- health-check.sh
+++ health-check.sh
@@ -3,6 +3,19 @@ set -x
 
 CODE=$(curl -s -o /dev/null -w %{http_code} http://${MESOS_AGENT_ENDPOINT%:*}:$PORT0/)
 
-[ $CODE -ge 200 -a $CODE -lt 300 -o $CODE -eq 503 ]
+if [ $CODE -ge 200 -a $CODE -lt 300 ]
+then
+  echo 0 > $MESOS_DIRECTORY/status
+else
+  if [ $CODE -eq 503 ]
+  then
+    if [ ! -f $MESOS_DIRECTORY/status ]
+    then
+      echo 1 > $MESOS_DIRECTORY/status
+    fi
+  else
+    echo 1 > $MESOS_DIRECTORY/status
+  fi
+fi
 
-exit $?
+exit $(cat $MESOS_DIRECTORY/status)
