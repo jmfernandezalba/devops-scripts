45a8f25329c0d1efae0fc972b6f9437401fedd65
Jose M. Fernandez-Alba <josemaria@starzplayarabia.com>
2018-05-22 13:31:27 +0200

    Create status directory inside temporal folder

--- health-check.sh
+++ health-check.sh
@@ -1,23 +1,25 @@
 
 set -x
 
-mkdir -p $MESOS_DIRECTORY
+export STATUS_DIRECTORY=/tmp$MESOS_DIRECTORY
+
+mkdir -p $STATUS_DIRECTORY
 
 CODE=$(curl -s -o /dev/null -w %{http_code} http://${MESOS_AGENT_ENDPOINT%:*}:$PORT0/)
 
 if [ $CODE -ge 200 -a $CODE -lt 300 ]
 then
-  echo 0 > $MESOS_DIRECTORY/status
+  echo 0 > $STATUS_DIRECTORY/status
 else
   if [ $CODE -eq 503 ]
   then
-    if [ ! -f $MESOS_DIRECTORY/status ]
+    if [ ! -f $STATUS_DIRECTORY/status ]
     then
-      echo 1 > $MESOS_DIRECTORY/status
+      echo 1 > $STATUS_DIRECTORY/status
     fi
   else
-    echo 1 > $MESOS_DIRECTORY/status
+    echo 1 > $STATUS_DIRECTORY/status
   fi
 fi
 
-exit $(cat $MESOS_DIRECTORY/status)
+exit $(cat $STATUS_DIRECTORY/status)
